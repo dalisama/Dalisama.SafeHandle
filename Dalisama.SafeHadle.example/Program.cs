c2f21ce2306b053fd5d580d25d889bd8e37f5c3e
kachbouri mohammed ali <med.ali.kachbouri@gmail.com>
2019-09-28 16:28:34 +0200

    Update Program.cs
    Fixing region description

--- Dalisama.SafeHadle.example/Program.cs
+++ Dalisama.SafeHadle.example/Program.cs
@@ -7,14 +7,14 @@ class Program
     static void Main()
     {
 
-        #region Using handle without safe Handle
+        #region Using handle/pointer without safe Handle
         using (var client = new ImageHandleClient("bat.jpg"))
         {
             client.CopyImageAndDoSth();
         }
         #endregion
 
-        #region Using handle without safe Handle
+        #region Using handle/pointer with safe Handle
         using (var client = new SafeImageHandleClient("bat.jpg"))
         {
             client.CopyImageAndDoSth();
