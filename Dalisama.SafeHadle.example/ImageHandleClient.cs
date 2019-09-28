4d982e7e7fa4ad111826e5f634498c0a11030cf1
Mohammed ali kachbouri <med.ali.kachbouri@hotmail.com>
2019-09-28 17:18:12 +0200

    removing the finalizer

--- Dalisama.SafeHadle.example/ImageHandleClient.cs
+++ Dalisama.SafeHadle.example/ImageHandleClient.cs
@@ -50,9 +50,6 @@ namespace Dalisama.SafeHadle.example
             disposed = true;
         }
 
-        ~ImageHandleClient()
-        {
-            Dispose(false);
-        }
+      
     }
 }
--- Dalisama.SafeHadle.example/SafeImageHandleClient.cs
+++ Dalisama.SafeHadle.example/SafeImageHandleClient.cs
@@ -52,9 +52,6 @@ namespace Dalisama.SafeHadle.example
             disposed = true;
         }
 
-        ~SafeImageHandleClient()
-        {
-            Dispose(false);
-        }
+      
     }
 }
